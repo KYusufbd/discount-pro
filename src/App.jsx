45a2d36724cc2aedf1a2115f798f9f6a4a863429
Kazi Yusuf <yfaka001@gmail.com>
2025-02-10 01:18:45 +0600

    Added themes to daisyUI

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 function App() {
 
   return (
-    <>
+    <div data-theme="dark">
       <h1 className="text-4xl text-center font-semibold">Welcome to Discount Pro</h1>
-    </>
+    </div>
   )
 }
 
